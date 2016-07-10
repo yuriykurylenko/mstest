94f2be50b41361440882d0d3ad9550d6a54fc0f8
Yuriy Kurylenko <yuriy.kurylenko@gmail.com>
2016-07-10 12:38:29 +0200

    Comment added.

--- MakeChange/MakeChange.js
+++ MakeChange/MakeChange.js
@@ -30,12 +30,18 @@ function MakeChange(N, denominations) {
     return false;
   }
 
+  // Sorting and filtering needed to handle the cases of unsorted array of denominations,
+  // and the array that may have duplicate values.
   denominations = denominations.sort(function(a,b) { 
     return b - a;
   }).filter(function(item, pos, self) {
     return self.indexOf(item) == pos;
   });
 
+  // Outer loop needed to handle the case with relatively prime denomination numbers, 
+  // f.ex., N = 54, denominations = [50, 27, 1].
+  // In this case the optimal change would be 2 bills - 2*27 
+  // (and not 5 - 1*50 + 4*1, as we will get as the result of applying only the inner loop).
   for (var i = 0; i < denominations.length; i++) {
     tempResult = 0;
     rest = N;
